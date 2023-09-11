67faa10568b1ea1320715829e65e3e3293dabf69
Madeleine Abraham <abraham.madeleine229@gmail.com>
2023-09-11 19:47:16 -0400

    created license badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "none")  {
+    return `![GitHub license](https://img.shields.io/badge/any_text-you_like-blue)`;
+
+  }
+  return ``;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
