1577578293356ec6e2f49fa9a14d553d79f57e28
Madeleine Abraham <abraham.madeleine229@gmail.com>
2023-09-11 17:17:26 -0400

    questions array

--- index.js
+++ index.js
@@ -3,7 +3,48 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your ReadMe?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'What is the description of your ReadMe?',
+    },
+    {
+        type: 'input',
+        name: 'License',
+        message: 'What license would you like to use?',
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Enter your Github username',
+    },
+    {
+        type: 'input',
+        name: 'Name',
+        message: 'Enter your full name',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter your email',
+    },
+    {
+        type: 'input',
+        name: 'Require',
+        message: 'List any dependencies used',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'List any additional contributors',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
