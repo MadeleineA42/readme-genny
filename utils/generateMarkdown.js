169fb5188bbe9126eee8b20c856eff395ad09127
Madeleine Abraham <abraham.madeleine229@gmail.com>
2023-09-12 16:40:33 -0400

    fixed paths for licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license !== "none")  {
-    return `![GitHub license](https://img.shields.io/badge/any_text-you_like-blue)`;
+  if (license !== "none") {
+    return `!![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
 
   }
   return ``;
@@ -11,13 +11,22 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license !== "none")
+  if (license !== "none") {
+    return `![GitHub license](https://img.shields.io/badge/license-$%7Blicense%7D-blue.svg)`
+  }
   return ``;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+
+This project is licensed under the terms of the ${license} license. ${renderLicenseLink(license)}`;
+  }
+  return ``;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
